1df1e2a9bf4369d29ee48a9b8418691976f6571a
Jarison Kleber Araujo <jarisonaraujo@gmail.com>
2020-06-09 10:35:08 -0300

    Adicionando teste GET e POST com mocha e chai

--- test/vehicle.spec.ts
+++ test/vehicle.spec.ts
@@ -1,18 +1,38 @@
 import  app  from '../src/server';
 import chaiHttp = require('chai-http');
-import 'chai';
+import chai from 'chai';
 import 'mocha';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
+
+describe('POST /veiculos', () => {
+  it('cadastra um novo veiculo', () => {
+    return chai.request(app)
+      .get('/veiculos')
+      .type('form')
+      .send({
+        'placa': 'ABC-1010',
+        'chassi': '123456789',
+        'renavam': 'ABC123456789',
+        'modelo': 'Corolla',
+        'marca': 'Toyota',
+        'ano': '2019'
+      })
+      .then(res => {
+        chai.expect(res.status).to.equal(200);
+      });
+  });
+});
+
  describe('GET /veiculos', () => {
   it('retorna lista de veiculos', () => {
     return chai.request(app)
       .get('/veiculos')
       .then(res => {
-        chai.expect(res.text).to.equal('kdkdkd');
+        chai.expect(res.status).to.equal(200);
       });
   });
 });
